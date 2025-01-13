5a0b3afe98db96935e576989102d579e56331ff0
drkveil <132784747+drkveil@users.noreply.github.com>
2025-01-13 04:15:33 +0100

    If and else

---
+++ if-else/script.js
@@ -0,0 +1,7 @@
+let javascriptOfficialName = prompt("What is the official name of Javascript?");
+
+if (javascriptOfficialName === "ECMAScript"){
+    alert("You got it right!");
+}else{
+    alert("You don't know ECMAScript?");
+}
