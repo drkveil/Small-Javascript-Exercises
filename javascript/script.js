f597d9e6a27a202b4793dd5e9cc5440c99e6b88d
Suleiman Yahaya <132784747+drkveil@users.noreply.github.com>
2025-01-23 21:07:40 +0100

    Function practice

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+function myProfile(name, age, grade){
+    console.log("Hi! " + name + ", just a reminder that you are " + age + " years old and your grade is " + grade);
+}
+
+myProfile("Suleiman", 25, 5);
+myProfile("Ali", 15, 4);
